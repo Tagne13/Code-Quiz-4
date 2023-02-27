81598eae0d95241ab57a427dfc8d603a33808f40
El Jefe <Tagne13@gmail.com>
2023-02-27 17:14:50 -0500

    added Q&A to JS, made comments for flow of JS file

---
+++ Assets/script.js
@@ -0,0 +1,81 @@
+// Variables for HTML Elements
+    // Time and score
+let timeEl = document.querySelector("p.timer");
+let secondsLeft = 75;
+let scoreEl = document.querySelector("p.score");
+
+// Sections
+    // Intro
+
+    // Questions
+
+    // Final
+
+    // High Scores
+
+//Buttons
+
+
+
+// Object for Q & A
+
+const questions = [
+    {
+        // Question 0
+        question: "Commonly used data types do NOT include:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer: "2"
+    },
+    {
+        // Question 1
+        question: "The condition in an if/else statement is enclosed within _____.",
+        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        // Question 2
+        question: "Arrays in JavaScript can be used to store _____.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        // Question 3
+        question: "String values must be closed within _____ when being assigned to variables.",
+        answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        // Question 4
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "3"
+    },
+    {
+        // Question 5
+        question: "Which of the following is used to define a variable in JavaScript?",
+        answers: ["1. var", "2. const", "3. let", "4. all of the above"],
+        correctAnswer: "3"
+    },
+];
+
+// Functions
+    // Timer
+
+    // Start quiz
+
+    // Set questions
+
+    // Check answer
+
+    // Add score
+
+    // Display score
+
+    // Save score
+
+    // Clear scores
+
+// Event Listeners
+
+
+
